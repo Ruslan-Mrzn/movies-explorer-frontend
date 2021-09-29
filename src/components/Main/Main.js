647ebe5e0d50336f07462e13edf5fd224eb6d10d
Ruslan <russl.murzin@gmail.com>
2021-09-29 23:25:49 +0300

    feat: добавить компоненты Portfolio и Footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,8 @@ import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 
 import './Main.css';
 
@@ -17,6 +19,8 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
+      <Footer />
     </>
   );
 }
