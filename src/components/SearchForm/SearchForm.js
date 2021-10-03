565aa0f5e2cfd9b3e3f6a1ec4ace7e7ba8ab57f8
Ruslan <russl.murzin@gmail.com>
2021-10-04 00:22:52 +0300

    feat: добавить компоненты SearchForm и FilterCheckbox

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,8 +7,10 @@ function SearchForm() {
   return (
     <section className="search">
       <form className="search__form">
-        <input className="search__input" type="text" placeholder="Фильм" />
-        <button className="button search__button" type="submit"></button>
+        <div className="search__wrapper">
+          <input className="search__input" type="text" placeholder="Фильм" />
+          <button className="button search__button" type="submit"></button>
+        </div>
         <FilterCheckbox />
       </form>
     </section>
